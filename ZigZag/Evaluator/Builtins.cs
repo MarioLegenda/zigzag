d70e962a2b2bd69b04738fa99e6faf2a185f7d5a
mario <marioskrlec222@proton.me>
2026-04-14 10:18:29 +0200

    Added more builtin functions

--- ZigZag/Evaluator/Builtins.cs
+++ ZigZag/Evaluator/Builtins.cs
@@ -21,7 +21,103 @@ public class Builtins
                 return new Integer(a.Value.Length);
             }
 
+            if (arg is Array b)
+            {
+                return new Integer(b.Elements.Count);
+            }
+
             return new Error("argument to `len` not supported, got {0}", args[0].Type());
+        });
+        
+        _dictionary["first"] = new Builtin((args) =>
+        {
+            if (args.Length != 1)
+            {
+                return new Error("wrong number of arguments. got={0}, want=1", args.Length);
+            }
+
+            IObject arg = args[0];
+            if (arg.Type() != ObjectTypeEnum.ARRAY_OBJ)
+            {
+                return new Error("argument to `first` must be ARRAY, got {0}", arg.Type());
+            }
+
+            Array array = (Array)arg;
+            if (array.Elements.Count > 0)
+            {
+                return array.Elements[0];
+            }
+
+            return new Error("argument to `len` not supported, got {0}", args[0].Type());
+        });
+        
+        _dictionary["last"] = new Builtin((args) =>
+        {
+            if (args.Length != 1)
+            {
+                return new Error("wrong number of arguments. got={0}, want=1", args.Length);
+            }
+
+            IObject arg = args[0];
+            if (arg.Type() != ObjectTypeEnum.ARRAY_OBJ)
+            {
+                return new Error("argument to `first` must be ARRAY, got {0}", arg.Type());
+            }
+
+            Array array = (Array)arg;
+            int length = array.Elements.Count;
+            if (length > 0)
+            {
+                return array.Elements[length - 1];
+            }
+
+            return new Error("argument to `len` not supported, got {0}", args[0].Type());
+        });
+        
+        _dictionary["rest"] = new Builtin((args) =>
+        {
+            if (args.Length != 1)
+            {
+                return new Error("wrong number of arguments. got={0}, want=1", args.Length);
+            }
+
+            IObject arg = args[0];
+            if (arg.Type() != ObjectTypeEnum.ARRAY_OBJ)
+            {
+                return new Error("argument to `first` must be ARRAY, got {0}", arg.Type());
+            }
+
+            Array array = (Array)arg;
+            int length = array.Elements.Count;
+            if (length > 0)
+            {
+                var newElements = array.Elements.Skip(1).ToList();
+                return new Array(newElements);
+            }
+
+            return new Null();
+        });
+        
+        _dictionary["push"] = new Builtin((args) =>
+        {
+            if (args.Length != 2)
+            {
+                return new Error("wrong number of arguments. got={0}, want=1", args.Length);
+            }
+
+            IObject arg = args[0];
+            if (arg.Type() != ObjectTypeEnum.ARRAY_OBJ)
+            {
+                return new Error("argument to `first` must be ARRAY, got {0}", arg.Type());
+            }
+
+            Array array = (Array)arg;
+            int length = array.Elements.Count;
+
+            var newElements = array.Elements.Append(args[1]).ToList();
+            Array newArray = new Array(newElements);
+
+            return newArray;
         });
     }
 
